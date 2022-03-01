ae683db60e8cea6161e7de8df35a0b4c02cc8224
npee <jmc4664@gmail.com>
2022-03-01 19:37:47 +0900

    Initial Commit

---
+++ src/main/java/io/npee/docker_jenkins_ci_cd/DockerJenkinsCiCdApplication.java
@@ -0,0 +1,13 @@
+package io.npee.docker_jenkins_ci_cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/npee/docker_jenkins_ci_cd/DockerJenkinsCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package io.npee.docker_jenkins_ci_cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
